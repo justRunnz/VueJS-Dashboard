bf4419784ed646e96f6f33215bcec218b223061a
elimf <elim.florvil@icloud.com>
2022-09-21 14:41:28 +0200

    splash screen

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/store">Store</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <LoadingScreen :isLoading="isLoading" />
+    <main v-if="!isLoading">
+      <HelloWorld />
+    </main>
+  </div>
 </template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import LoadingScreen from "./components/LoadingScreen";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    LoadingScreen,
+  },
+  data() {
+    return { isLoading: true };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  },
+};
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="{ loader: true, fadeout: !isLoading }">
+    <img src="../assets/sneakers.png" alt="" />
+    Sneakers
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingScreen",
+  props: ["isLoading"],
+};
+</script>
+
+<style>
+.loader {
+  background-color: #ab7d63;
+  bottom: 0;
+  color: white;
+  font-size: 32px;
+  left: 0;
+  overflow: hidden;
+  padding-top: 10vh;
+  position: fixed;
+  right: 0;
+  text-align: center;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.fadeout {
+  animation: fadeout 2s forwards;
+}
+
+@keyframes fadeout {
+  to {
+    opacity: 0;
+    visibility: hidden;
+  }
+}
+</style>
