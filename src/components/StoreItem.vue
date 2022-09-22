d5d1e21865595ff97287120050f2647e2c6a78ad
IlanP <ilan.petiot@icloud.com>
2022-09-23 00:06:59 +0200

    📝 - Replace words by svg

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -112,8 +112,12 @@
           {{ product.price }}€
         </td>
         <td class="py-4 px-6">
-          <label  class="font-medium text-red-600 dark:text-red-500 hover:underline" @click="deleteProducts(product.id)">Remove</label> /
-          <label class="font-medium text-blue-600 dark:text-red-500 hover:underline " @click="getProduct(product.id)" data-bs-toggle="modal" data-bs-target="#staticBackdropModify">Modify</label>
+          <label  class="font-medium text-red-600 dark:text-red-500 hover:underline cursor-pointer " @click="deleteProducts(product.id)">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+          </label>
+          <label class="font-medium text-blue-600 dark:text-red-500 hover:underline cursor-pointer" @click="getProduct(product.id)" data-bs-toggle="modal" data-bs-target="#staticBackdropModify">
+            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+          </label>
         </td>
       </tr>
 
