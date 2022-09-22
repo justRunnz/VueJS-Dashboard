47300daedc13137851c003b00ec8225a7478cb7d
justRunnz <ruigaspar03@hotmail.com>
2022-09-22 23:15:50 +0200

    search bars visual

--- src/components/StoreItem.vue
+++ src/components/StoreItem.vue
@@ -3,19 +3,40 @@
     <h1>Products</h1>
   </div>
 
-  <div class="newItem">
+  <div div class="newItem">
     <!-- Shearch Bar Input Modal -->
 
-    <input
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-64 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      placeholder="Search a product"
-      v-model="textSearch"
-      @keyup="searchProduct()"
-    />
+    <div class="relative">
+      <div class="relative w-full">
+        <div
+          class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"
+        >
+          <svg
+            aria-hidden="true"
+            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </div>
+        <input
+          class="g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search a product"
+          v-model="textSearch"
+          @keyup="searchProduct()"
+        />
+      </div>
+    </div>
 
     <div class="flex">
       <input
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-28 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        class="g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         name="priceMin"
         type="number"
         min="0"
@@ -23,7 +44,7 @@
         v-model="textSearchPriceMin"
       />
       <input
-        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-28 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        class="g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         name="priceMax "
         type="number"
         min="0"
@@ -31,7 +52,7 @@
         v-model="textSearchPriceMax"
       />
       <input
-        class="inline-block px-2 py-2.5 bg-blue-600 text-white font-bold text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+        class="inline-block px-2 py-2.5 bg-blue-600 text-white font-bold text-xs leading-tight ml-2 uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         type="button"
         name="stockMin"
         min="0"
@@ -42,10 +63,10 @@
 
     <select
       id="countries"
-      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-52 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      class="g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-52 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       @change="onChange($event)"
     >
-      <option selected="">Choose a state</option>
+      <option value="default" selected="">Choose a state</option>
       <option value="inStock">In Stock</option>
       <option value="outOfStock">Out of Stock</option>
       <option value="almostOutOfStock">Almost Out of Stock</option>
@@ -516,7 +537,6 @@ export default {
     },
 
     onChange(event) {
-      console.log(event.target.value);
       if (event.target.value == "inStock") {
         this.filteredProducts = this.products.filter(
           (product) => product.stock >= 10
@@ -529,6 +549,8 @@ export default {
         this.filteredProducts = this.products.filter(
           (product) => product.stock <= 0
         );
+      } else if (event.target.value == "default") {
+        this.filteredProducts = this.products;
       }
     },
   },
