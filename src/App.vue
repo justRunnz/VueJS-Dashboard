a86dd70387f24af1dd94d73b3bd1128332fabaa2
IlanP <ilan.petiot@icloud.com>
2022-09-22 10:59:21 +0200

    ✏️ - Add all components in app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/store">Store</router-link>
-  </nav>
-  <router-view/>
 
-  <div id="app">
+  <Header/>
+  <div class="container-content flex">
+    <nav>
+      <SideBar/>
+    </nav>
+    <router-view/>
+  </div>
+
+  <div id="loadingScreen">
     <LoadingScreen :isLoading="isLoading" />
     <main v-if="!isLoading"></main>
   </div>
@@ -14,11 +16,15 @@
 
 <script>
 import LoadingScreen from "./components/LoadingScreen";
+import SideBar from "./components/SideBar";
+import Header from "@/components/HeaderComponent";
 
 export default {
   name: "App",
   components: {
+    Header,
     LoadingScreen,
+    SideBar,
   },
   data() {
     return { isLoading: true };
@@ -33,11 +39,16 @@ export default {
 
 <style>
 #app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container-content {
+  width: 100% ;
+  min-height: 95vh;
 }
 </style>
