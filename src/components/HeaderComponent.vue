e1ae47b3560fc1d1f20f5181f869084474a3faec
IlanP <ilan.petiot@icloud.com>
2022-09-22 10:55:35 +0200

    ✏️ - Create HeaderComponent.vue

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+    logo
+  </div>
+</template>
+
+<style>
+  .header{
+    background-color: #ab7d63;
+    color: white;
+    font-size: 32px;
+    padding: 10px;
+    text-align: center;
+  }
+</style>
