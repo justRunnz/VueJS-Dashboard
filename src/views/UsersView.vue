a3769f4157f10672c60894de215bc090b3d6cb51
IlanP <ilan.petiot@icloud.com>
2022-09-23 00:06:31 +0200

    ✏️ - Add View for users

---
+++ src/views/UsersView.vue
@@ -0,0 +1,27 @@
+<template>
+  <DashboardUsers/>
+</template>
+
+<script>
+// @ is an alias to /src
+import DashboardUsers from "@/components/DashboardUsers.vue";
+export default {
+  name: "UsersView",
+  components: {
+    DashboardUsers,
+  },
+};
+</script>
+
+<style>
+.right-container {
+  display: flex;
+  /*align-items: center ;*/
+  justify-content: center;
+  width: 100%;
+  /*margin-left: 10em;*/
+}
+.productContainer {
+  width: 80%;
+}
+</style>
