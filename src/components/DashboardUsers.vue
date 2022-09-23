568ad8518e02b2ef9505fa7f25d0e48b4f8557b8
IlanP <ilan.petiot@icloud.com>
2022-09-23 13:57:22 +0200

    📝 - Change placeholder

--- src/components/DashboardUsers.vue
+++ src/components/DashboardUsers.vue
@@ -89,7 +89,7 @@
                       id="image"
                       v-model="formProduct.firstname"
                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="https://www.image-link.png"
+                      placeholder=" Write your first name"
                     />
                   </div>
                   <div class="mb-6">
@@ -103,7 +103,7 @@
                       id="color"
                       v-model="formProduct.lastname"
                       class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder=" write your last name"
+                      placeholder=" Write your last name"
                     />
                   </div>
                   <div class="mb-6">
