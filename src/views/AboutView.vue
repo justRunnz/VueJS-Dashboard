d6681445b029772129df8b486323738a14e22ed5
elimf <elim.florvil@icloud.com>
2022-09-23 10:25:15 +0200

    pagination done v2

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,31 +1,5 @@
 <template>
-  <div class="right-container">
-    <div class="productContainer">
-      <StoreItem />
-    </div>
+  <div class="about">
+    <h1>This is an about page</h1>
   </div>
 </template>
-<script>
-// @ is an alias to /src
-import StoreItem from "@/components/StoreItem.vue";
-
-export default {
-  name: "StoreView",
-  components: {
-    StoreItem,
-  },
-};
-</script>
-
-<style>
-.right-container {
-  display: flex;
-  /*align-items: center ;*/
-  justify-content: center;
-  width: 100%;
-  /*margin-left: 10em;*/
-}
-.productContainer {
-  width: 80%;
-}
-</style>
